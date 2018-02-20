3ec93985cc35583fec0dd3354a8ba3d8494f2073
Charity <Youngblood.charity@gmail.com>
2018-02-20 11:28:26 -0800

    added comments on superclass; added IBAction connection for touchCard

--- Concentration/Controller/ConcentrationViewController.swift
+++ Concentration/Controller/ConcentrationViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ConcentrationViewController: UIViewController { //UIViewController is the superclass of ConcentrationViewController - REMEMEBER the superclass is the "main" class that other classes inherit from
-    // the class UIViewController is in UIKit - and knows everything about controlling the UI of the application
+    // the class UIViewController is in UIKit - and knows everything about controlling the UI of the application - since our ConcentrationViewController "inherits" everything from the UIViewController superclass, it now has access to everything the UIViewController has access to
 
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction func touchCard(_ sender: UIButton) { // in Swift, ever argument/parameter has a name in front of the data type that you include when you call the method
+        // this actually equates to two names: an internal name - the name we use inside of the implementation (inside the curly braces)
+        // external name: the name callers use
+        
+        print("the card is being touched")
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
